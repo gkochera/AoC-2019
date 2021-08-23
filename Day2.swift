70d158f0f0dba6d6ffa814a85572fc583b9ddc93
George Kochera <george.kochera@gmail.com>
2021-08-22 21:59:19 -0400

    created day 2

---
+++ Day2.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  AoC-2019
+//
+//  Created by George J Kochera III on 8/22/21.
+//
+
+import Foundation
+
+class Day2 {
+    
+    // Have an flag on construction for debugging statements
+    let debug: Bool
+    init(debug: Bool) {
+        self.debug = debug
+    }
+    
+    func solve(input: [Float]) -> Void {
+
+    }
+    
+}
